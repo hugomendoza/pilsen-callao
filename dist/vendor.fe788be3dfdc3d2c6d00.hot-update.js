a2492be90132530ea9feb11776aee2b70547e1bd
Hugo Mendoza <hmendoza@serempre.com>
2020-06-24 19:03:41 -0500

    banner home desktop

---
+++ dist/vendor.fe788be3dfdc3d2c6d00.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate("vendor",{
+
+/***/ "./node_modules/css-loader/dist/runtime/api.js":
+false,
+
+/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJ2ZW5kb3IuZmU3ODhiZTNkZmRjM2QyYzZkMDAuaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,11 @@ module.exports = {
   plugins: [
     require("tailwindcss")("./tailwind.config.js"),
     require("postcss-preset-env")({
+      browsers: "last 2 versions",
       autoprefixer: { grid: true },
       features: {
         "nesting-rules": true,
+        "custom-selectors": true,
       },
     }),
   ],
